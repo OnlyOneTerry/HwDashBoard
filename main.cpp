67672fb81bc0c0c0b6c73c17c2cadc9468d95d85
tangrui <1269766350@qq.com>
2024-10-30 10:26:44 +0800

    delete discard code

--- main.cpp
+++ main.cpp
@@ -3,31 +3,8 @@
 #include "CanUtil.h"
 #include <functional>
 
-//#include "component/battery_cluster_comp/battery_cluster_comp.hpp"
-
-//using namespace srvcomps;
-
-//DataHandler* data_handler_ =nullptr;
-
 int main(int argc, char *argv[])
 {
-//   BatteryClusterComp bcc;
-
-//   HandlerCb func =[](const void* arg1,void* arg2)
-//   {
-//      std::cout<<"arg1 is :"<<arg1<<"arg2 is :"<<arg2<<std::endl;
-//   };
-
-//   void* arg1 = nullptr;
-//   void* arg2 = nullptr;
-
-//   data_handler_ = new DataHandler(func,arg1,arg2);
-
-//   bcc.RegisterDataHandler(data_handler_);
-//   bcc.Create();
-
-//    ServiceLifeCycle life;
-//    bcc.Consume(life);
 
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
